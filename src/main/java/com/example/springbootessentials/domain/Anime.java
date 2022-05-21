f6a70a8d10c0026d58f1776b1630a07e19136cba
Bruno Totoli <santosbruno101062@gmail.com>
2022-05-21 12:32:51 -0300

    Jpa Setup

--- src/main/java/com/example/springbootessentials/domain/Anime.java
+++ src/main/java/com/example/springbootessentials/domain/Anime.java
@@ -2,13 +2,22 @@ package com.example.springbootessentials.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Data
 @ToString
 @AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Anime {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 
