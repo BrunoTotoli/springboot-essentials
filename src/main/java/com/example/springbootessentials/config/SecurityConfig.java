b9c04ade6077fde77be7e67e11ae636da46ab6af
Bruno Totoli <santosbruno101062@gmail.com>
2022-08-16 07:11:01 -0300

    PreAuthorize to roles security

--- src/main/java/com/example/springbootessentials/config/SecurityConfig.java
+++ src/main/java/com/example/springbootessentials/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.example.springbootessentials.config;
 
 import lombok.extern.log4j.Log4j2;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -11,11 +12,15 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 @Log4j2
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
+        http.csrf().disable()
+//                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+//                .and()
+                .authorizeRequests()
                 .anyRequest()
                 .authenticated()
                 .and()
--- src/main/java/com/example/springbootessentials/controller/AnimeController.java
+++ src/main/java/com/example/springbootessentials/controller/AnimeController.java
@@ -2,17 +2,18 @@ package com.example.springbootessentials.controller;
 
 
 import com.example.springbootessentials.domain.Anime;
-import com.example.springbootessentials.util.DateUtil;
 import com.example.springbootessentials.requests.AnimePostRequestBody;
 import com.example.springbootessentials.requests.AnimePutRequestBody;
+import com.example.springbootessentials.service.AnimeService;
+import com.example.springbootessentials.util.DateUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
-import com.example.springbootessentials.service.AnimeService;
 
 import javax.validation.Valid;
 import java.time.LocalDateTime;
@@ -51,6 +52,7 @@ public class AnimeController {
     }
 
     @PostMapping
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<Anime> save(@RequestBody @Valid AnimePostRequestBody animePostRequestBody) {
         return new ResponseEntity<>(animeService.save(animePostRequestBody), HttpStatus.CREATED);
     }
